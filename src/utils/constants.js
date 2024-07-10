175e193bdf555b19638f7849a94085faaf2ca8bc
RItu Raj <rajritu2084@gmail.com>
2024-07-11 00:04:31 +0530

    Updated restaurant api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const CDN_URL =
   'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
 export const RESTAURANT_URL =
-  'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+  'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 export const MENU_API_URL =
   'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.45970&lng=77.02820&restaurantId=';
